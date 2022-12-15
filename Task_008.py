c3ce65747975fa2cb749817f3c0c6a5efd2e0700
Julia Ch <juliecha@yandex.ru>
2022-12-15 12:58:18 +0300

    UPD Task_008

--- Task_008.py
+++ Task_008.py
@@ -3,3 +3,21 @@
 # - x=34; y=-30    -> 4
 # - x=2; y=4       -> 1
 # - x=-34; y=-30   -> 3
+
+
+coordinateX = int(input('Введите координату X: '))
+coordinateY = int(input('Введите координату Y: '))
+if coordinateX > 0 and coordinateY > 0:
+    print('Точка находится в 1 четверти плоскости')
+elif coordinateX < 0 and coordinateY > 0:
+    print('Точка находится в 2 четверти плоскости')
+elif coordinateX < 0 and coordinateY < 0:
+    print('Точка находится в 3 четверти плоскости')
+elif coordinateX > 0 and coordinateY < 0:
+    print('Точка находится в 4 четверти плоскости')
+elif coordinateX == 0 and coordinateY != 0:
+    print('Точка находится на оси абсцисс')
+elif coordinateX != 0 and coordinateY == 0:
+    print('Точка находится на оси ординат')
+else:
+    print('Точка находится в начале координат')
