a89752c62078fbd582fc332de9c40f2fcb1dd963
Julia Ch <juliecha@yandex.ru>
2022-12-15 13:31:38 +0300

    UPD Task_010

--- Task_010.py
+++ Task_010.py
@@ -2,3 +2,13 @@
 # Пример:
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
+
+import math
+
+coordinateXA = int(input('Введите координату X точки A: '))
+coordinateYA = int(input('Введите координату Y точки A: '))
+coordinateXB = int(input('Введите координату X точки B: '))
+coordinateYB = int(input('Введите координату Y точки B: '))
+
+distance = math.sqrt((coordinateXA - coordinateXB)**2 + (coordinateYA - coordinateYB)**2)
+print(round(distance, 2))
