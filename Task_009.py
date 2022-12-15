f01c7a92db5a6622d7684b51cce6fb31c4bf0da6
Julia Ch <juliecha@yandex.ru>
2022-12-15 13:03:30 +0300

    UPD Task_009

--- Task_009.py
+++ Task_009.py
@@ -1,1 +1,14 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+coordinate = int(input('Введите номер четверти плоскости: '))
+if coordinate == 1:
+    print('x > 0; y > 0')
+elif coordinate == 2:
+    print('x < 0; y > 0')
+elif coordinate == 3:
+    print('x < 0; y < 0')
+elif coordinate == 4:
+    print('x > 0; y < 0')
+else:
+    print('Вы ввели некоррекное значение')
