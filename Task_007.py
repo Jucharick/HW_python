34017ea4870ab9b5fa48b0a3bdedca61f7ca8432
Julia Ch <juliecha@yandex.ru>
2022-12-18 23:17:10 +0300

    Add True - False

--- Task_007.py
+++ Task_007.py
@@ -5,3 +5,11 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             print(not (x or y or z) == (not x and not y and not z))
+
+print()
+# вариант преподавателя
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            expression = not (x or y or z) == (not x and not y and not z)
+            print(f'{x}, {y}, {z},   =>  результат {expression}')
