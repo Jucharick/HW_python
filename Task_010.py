e35db3ee056230a3930c9d8760407b97cff1730c
Julia Ch <juliecha@yandex.ru>
2022-12-18 23:07:32 +0300

    Add split()

--- Task_010.py
+++ Task_010.py
@@ -12,3 +12,11 @@ coordinateYB = int(input('Введите координату Y точки B: ')
 
 distance = math.sqrt((coordinateXA - coordinateXB)**2 + (coordinateYA - coordinateYB)**2)
 print(round(distance, 2))
+
+
+# вариант преподавателя
+first = input('Введите координаты точки А через пробел: ').split(' ')
+second = input('Введите координаты точки B через пробел: ').split() # split() по умолчанию ставит пробел
+
+distance1 = (float(first[0]) - float(second[0]))**2 + (float(first[1]) - float(second[1]))**2 
+print(f'Расстояние между точками А и В равно => {round(math.sqrt(distance1), 2)}')
