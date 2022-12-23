e3a5ebba84c2a00c465c5966b7058faf13e6a872
Julia Ch <juliecha@yandex.ru>
2022-12-23 22:00:26 +0300

    UPD Task_020

--- Task_020.py
+++ Task_020.py
@@ -3,12 +3,13 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 import math
+from random import randint
 
 number = int(input('Введите число элементов в списке: '))
 my_list = []
 sum = 0
 for i in range(number):
-    my_list.append(int(input('Введите число: ')))
+    my_list.append(randint(0, 7))
 res = []
 for i in range(math.ceil((number/2))): # math.ceil() округление в большую сторону
     res.append(my_list[i]*my_list[number - 1 - i])
