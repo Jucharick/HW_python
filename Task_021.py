9cb0ad333c825796b66a65d0ce3489ccfcae64ac
Julia Ch <juliecha@yandex.ru>
2022-12-24 13:28:36 +0300

    UPD Task_021 - разница между максимальным и минимальным значением дробной части элементов

--- Task_021.py
+++ Task_021.py
@@ -3,7 +3,7 @@
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
 import random
-import decimal
+from decimal import Decimal
 
 number = int(input('Введите число элементов в списке: '))
 my_list = []
@@ -12,16 +12,20 @@ for i in range(number):
     my_list.append(round(random.uniform(0, 10), amount))
 print(my_list)
 
-min = 0
-max = 0
+new_list = []
 for i in range(len(my_list)):
     if my_list[i] != int(my_list[i]):
-        part = my_list[i] - int(my_list[i])
-        print(part)
-        if part > max:
-            max = part
-        if part < min:
-            min = part
+        part = round(my_list[i]- int(my_list[i]), 3) # не знаю как корректно округлить дробную часть, взяла максимальное из amount
+        new_list.append(part)
+print(f'{my_list}  =>  {round((max(new_list) - min(new_list)), 3)}')
 
-print(max)
-print(min)
+
+# не используя методы max() и min()
+max = new_list[0]
+min = new_list[0]
+for i in range(len(new_list)):
+    if new_list[i] < min:
+        min = new_list[i]
+    if new_list[i] > max:
+        max = new_list[i]
+print(f'{my_list}  =>  {round((max - min), 3)}')
