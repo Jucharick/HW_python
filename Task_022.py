c4b5988e322169ff409f756e6899c121f8337eca
Julia Ch <juliecha@yandex.ru>
2022-12-24 12:55:37 +0300

    UPD Task_022 - решение через строку

--- Task_022.py
+++ Task_022.py
@@ -6,13 +6,22 @@
 
 
 num = int(input('Введите число: '))
+num_2 = num
+
+# список
 res = []
-
 print(f'{num}  => ', end = ' ')
-
 while int(num) > 0:
     res.insert(0, (int(num % 2))) # insert() - добавляет элемент на указанный индекс (в данном случае на индекс 0)
     num = num / 2
-
 for i in res:
     print(i, end = '')
+print()
+
+# строка
+my_str = ''
+print(f'{num_2} ', end = ' ')
+while int(num_2) > 0:
+    my_str = str(num_2 % 2) + my_str # добавляю в начало строки my_str
+    num_2 = num_2 // 2
+print(f'=>  {my_str}')
