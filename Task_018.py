cdcd00353a725bcf093870e07b81f2bf47fb3f4a
Julia Ch <juliecha@yandex.ru>
2022-12-25 11:32:46 +0300

    UPD Task_018 - shuffle_list

--- Task_018.py
+++ Task_018.py
@@ -22,8 +22,25 @@ for i in range(num):
     my_list1.append(i)
 print(my_list1)
 for i in range(num):
-    new_i = randint(0, num - 1)
+    new_i = randint(0, num - 1) # при использовании этого метода есть вероятность что элементы заменятся сами на себя
     temp = my_list1[i]
     my_list1[i] = my_list1[new_i]
     my_list1[new_i] = temp
 print(my_list1)
+
+# вариант преподавателя
+import datetime
+
+def random_int(num):
+    rand = datetime.datetime.now().microsecond/10**6  # datetime.datetime.now().microsecond вернет микросекунды;  делим на миллион, потому что нам надо получить дробь
+    return int(num*rand)
+
+list_num = [i for i in range(number+1)] # создаем список от 0 до введенного number
+print(list_num)
+
+for i in range((len(list_num)-1), -1, -1): # идем до -1 потому что range не включает последний элемент
+    j = random_int(i+1)
+    list_num[i], list_num[j] = list_num[j], list_num[i] # замена местами элементов как в C# через temp
+
+print(list_num)
+
