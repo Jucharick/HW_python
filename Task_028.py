3b6888f8a012aae0b6e8786cf30e5241be04bd06
Julia Ch <juliecha@yandex.ru>
2022-12-26 21:59:18 +0300

    UPD Task_028

--- Task_028.py
+++ Task_028.py
@@ -9,26 +9,42 @@ def Read_file(name_file: str) -> str:
     data.close()
     return str
 
-def Create_dict_equation(str: str) -> dict:
+def Create_dict_polynomial(new_string: str) -> dict:
     equation = {}
-
+    polynomial = new_string.replace(' ', '').replace('=0', ''). replace('+', ' ').split()
+    len_str = len(polynomial) - 1
+    for i in range(len_str, -1, -1):
+        if polynomial[len_str - i].startswith('x'):
+            equation[i] = polynomial[len_str - i]
+        else: 
+            equation[i] = int(polynomial[len_str - i].split('*')[0])
     return equation
 
+def Sum_of_polynomials(dict_1: dict, dict_2: dict) -> dict:
+    sum_pol = {}
+    if len(dict_1) > len(dict_2):
+        for k, v in dict_1.items(): # item() возвращает объект представления, который отображает список пары кортежей (ключ, значение) данного словаря
+            sum_pol[k] = dict_1.get(k, v) + dict_2.get(k, v)
+    if len(dict_1) < len(dict_2):
+        for k, v in dict_2.items(): # item() возвращает объект представления, который отображает список пары кортежей (ключ, значение) данного словаря
+            sum_pol[k] = dict_1.get(k, v) + dict_2.get(k, v)
+    return sum_pol
+        
 
 
 
-
-
-
+print('Первый многочлен')
 eq_str_1 = Read_file('equation_1.txt')
 print(eq_str_1)
-equation_1 = Create_dict_equation(eq_str_1)
-print(equation_1)
+polynomial_1 = Create_dict_polynomial(eq_str_1)
+print(polynomial_1)
 
-
-
-
+print('Второй многочлен')
 eq_str_2 = Read_file('equation_2.txt')
 print(eq_str_2)
-equation_2 = Create_dict_equation(eq_str_2)
-print(equation_2)
+polynomial_2 = Create_dict_polynomial(eq_str_2)
+print(polynomial_2)
+
+print('Сумма многочленов')
+sum_pol = Sum_of_polynomials(polynomial_1, polynomial_2)
+print(sum_pol)
