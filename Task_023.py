7d0b17c0f52a4f96f19507887ccc768e2a785c5b
Julia Ch <juliecha@yandex.ru>
2022-12-26 17:59:42 +0300

    UPD Task_023 - negafibonacci

--- Task_023.py
+++ Task_023.py
@@ -6,11 +6,11 @@
 num = int(input('Введите число k: '))
 res = []
 list_1 = [0, 1]
-for i in range(2, num+1):
-    list_1.append(list_1[i-2] + list_1[i-1])
 list_2 = [1, 0]
 for i in range(2, num+1):
+    list_1.append(list_1[i-2] + list_1[i-1])
     list_2.insert(0, list_2[i-len(list_2)+1] - list_2[i-i])
+    
 negafibonacci = list_2 + list_1
 negafibonacci.remove(0) # удаляю первый совпадение = 0
 print(negafibonacci)
