02a53b9b4f0a2efe1934c26f6e3086fe25f60bb3
Julia Ch <juliecha@yandex.ru>
2022-12-27 21:20:11 +0300

    UPD Task_029 - add minus

--- Task_029.py
+++ Task_029.py
@@ -17,7 +17,7 @@ import random
 def Create_dict(k: int) -> dict:
     equation = {}
     for i in range(k, -1, -1): # до степени 0 => граница -1 не включается
-        equation[i] = random.randint(-100, 10)
+        equation[i] = random.randint(-10, 10)
     return equation
 
 # создается строка - многочлен
@@ -62,11 +62,13 @@ def Read_file(name_file: str) -> str:
 # преобразование строки в словарь
 def Create_dict_polynomial(new_string: str) -> dict:
     equation = {}
-    polynomial = new_string.replace(' ', '').replace('=0', ''). replace('+', ' ').split()
+    polynomial = new_string.replace(' ', '').replace('=0', ''). replace('+', ' ').replace('-', ' -').split()
     len_str = len(polynomial) - 1
     for i in range(len_str, -1, -1):
         if polynomial[len_str - i].startswith('x'):
             equation[i] = polynomial[len_str - i]
+        # elif polynomial[len_str - i].startswith('-x'):
+        #     equation[i] = polynomial[len_str - i]*(-1)
         else: 
             equation[i] = int(polynomial[len_str - i].split('*')[0])
     return equation
@@ -78,9 +80,11 @@ def Sum_of_polynomials(dict_1: dict, dict_2: dict) -> dict:
         for k, v in dict_1.items(): # item() возвращает объект представления, который отображает список пары кортежей (ключ, значение) данного словаря
             if k in dict_1 and k not in dict_2:
                 sum_pol[k] = dict_1.get(k, v)
+            elif v < 0:
+                sum_pol[k] = dict_1.get(k, v) - dict_2.get(k, v)
             else:
                 sum_pol[k] = dict_1.get(k, v) + dict_2.get(k, v)
-    if len(dict_1) < len(dict_2):
+    if len(dict_1) <= len(dict_2):
         for k, v in dict_2.items(): # item() возвращает объект представления, который отображает список пары кортежей (ключ, значение) данного словаря
             if k in dict_2 and k not in dict_1:
                 sum_pol[k] = dict_2.get(k, v)
@@ -92,31 +96,36 @@ def Sum_of_polynomials(dict_1: dict, dict_2: dict) -> dict:
 # создание двух многочленов и запись в два файла
 k_1 = int(input('Введите максимальную степень для многочлена: '))
 equation_1 = Create_dict(k_1)
+print(equation_1)
 eq_str_1 = Create_string(equation_1)
 print(eq_str_1)
-# Write_file(eq_str_1,'equation_1.txt')
+Write_file(eq_str_1,'equation_1.txt')
 
-# k_2 = int(input('Введите максимальную степень для второго многочлена: '))
-# equation_2 = Create_dict(k_2)
-# eq_str_2 = Create_string(equation_2)
-# print(eq_str_2)
-# Write_file(eq_str_2, 'equation_2.txt')
+k_2 = int(input('Введите максимальную степень для второго многочлена: '))
+equation_2 = Create_dict(k_2)
+print(equation_2)
+eq_str_2 = Create_string(equation_2)
+print(eq_str_2)
+Write_file(eq_str_2, 'equation_2.txt')
 
 
-# # чтение двух многочленов из двух файлов, суммирование словарей, запись суммы в файл
-# print('Первый многочлен')
-# eq_str_1 = Read_file('equation_1.txt')
-# print(eq_str_1)
-# polynomial_1 = Create_dict_polynomial(eq_str_1)
+# чтение двух многочленов из двух файлов, суммирование словарей, запись суммы в файл
+print('Первый многочлен')
+eq_read_1 = Read_file('equation_1.txt')
+print(eq_read_1)
+polynomial_1 = Create_dict_polynomial(eq_read_1)
+print(polynomial_1)
 
-# print('Второй многочлен')
-# eq_str_2 = Read_file('equation_2.txt')
-# print(eq_str_2)
-# polynomial_2 = Create_dict_polynomial(eq_str_2)
+print('Второй многочлен')
+eq_read_2 = Read_file('equation_2.txt')
+print(eq_str_2)
+polynomial_2 = Create_dict_polynomial(eq_read_2)
+print(polynomial_2)
 
-# print('Сумма многочленов')
-# sum_pol = Sum_of_polynomials(polynomial_1, polynomial_2)
-# str_sum_of_polynomials = Create_string(sum_pol)
-# print(str_sum_of_polynomials)
+print('Сумма многочленов')
+sum_pol = Sum_of_polynomials(polynomial_1, polynomial_2)
+print(sum_pol)
+str_sum_of_polynomials = Create_string(sum_pol)
+print(str_sum_of_polynomials)
 
 # Write_file(str_sum_of_polynomials, 'sum_of_polynomials.txt')
