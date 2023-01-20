86a031c73595f4397f23bdb1628479260bffea39
Julia Ch <juliecha@yandex.ru>
2023-01-20 19:01:20 +0300

    UPD Task_019 - add list comprehension

--- Task_019.py
+++ Task_019.py
@@ -3,11 +3,21 @@
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
 number = int(input('Введите число элементов в списке: '))
-my_list = []
-sum = 0
-for i in range(number):
-    my_list.append(int(input('Введите число: ')))
+
+# sum = 0
+
+# my_list = []
+# for i in range(number):
+#     my_list.append(int(input('Введите число: ')))
+# print(my_list)
+# for i in range(1, number, 2):
+#     sum = sum + my_list[i]
+# print(f'Сумма элементов списка, стоящих на позиции с нечетным индексом, => {sum}')
+
+
+# Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension.
+
+my_list = [int(input('Введите число: ')) for _ in range(number) ]
 print(my_list)
-for i in range(1, number, 2):
-    sum = sum + my_list[i]
-print(f'Сумма элементов списка, стоящих на позиции с нечетным индексом, => {sum}')
+sun_odd_index = sum([my_list[i] for i in range(len(my_list)) if i % 2 !=0])
+print(sun_odd_index)
