c8eece0b0815c1286e3839d431ed216465e39f55
Julia Ch <juliecha@yandex.ru>
2023-01-21 18:24:48 +0300

    UPD Task_025

--- Task_025.py
+++ Task_025.py
@@ -1,8 +1,10 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+
 num = int(input('Введите число N: '))
 list_nums = []
 divider = 1
+
 for i in range(num):
     divider = divider + 1
     if int(num) % divider == 0:
@@ -10,3 +12,4 @@ for i in range(num):
         num //= divider
         divider = 1
 print(f'список простых множителей числа N => {list_nums}')
+
