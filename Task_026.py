794a4d04fc592d9ca22b7784ca105021512f54fe
Julia Ch <juliecha@yandex.ru>
2023-01-21 19:11:35 +0300

    UPD Task_026

--- Task_026.py
+++ Task_026.py
@@ -1,14 +1,25 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 import random
 
-number = int(input('Введите число элементов в списке: '))
-my_list = []
-for i in range(number):
-    my_list.append(random.randint(0, 5))
-print(my_list)
+# number = int(input('Введите число элементов в списке: '))
+# my_list = []
+# for i in range(number):
+#     my_list.append(random.randint(0, 5))
+# print(my_list)
 
+# new_list = []
+# for i in range(len(my_list)):
+#     if my_list[i] not in new_list:
+#         new_list.append(my_list[i])
+# print(new_list)
+
+
+# Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension.
+
+
+number = int(input('Введите число элементов в списке: '))
+my_list = [random.randint(0, 5) for i in range(number)]
+print(my_list)
 new_list = []
-for i in range(len(my_list)):
-    if my_list[i] not in new_list:
-        new_list.append(my_list[i])
+new_list = [new_list.append(my_list[i]) for i in range(len(my_list)) if my_list[i] not in new_list]
 print(new_list)
