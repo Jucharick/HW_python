9810bca0bba821ea1cfd0bd5054fe8af260eea79
Julia Ch <juliecha@yandex.ru>
2023-01-21 21:15:40 +0300

    UPD Task_026 - add list comprehension

--- Task_026.py
+++ Task_026.py
@@ -1,14 +1,26 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
-import random
+from random import randint
+
+# number = int(input('Введите число элементов в списке: '))
+# my_list = []
+# for i in range(number):
+#     my_list.append(randint(0, 5))
+# print(my_list)
+
+# new_list = []
+# for i in range(len(my_list)):
+#     if my_list[i] not in new_list:
+#         new_list.append(my_list[i])
+# print(new_list)
+
+
+# Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension.
 
 number = int(input('Введите число элементов в списке: '))
-my_list = []
-for i in range(number):
-    my_list.append(random.randint(0, 5))
+my_list = [randint(0, 5) for _ in range(number)]
 print(my_list)
 
 new_list = []
-for i in range(len(my_list)):
-    if my_list[i] not in new_list:
-        new_list.append(my_list[i])
+new_list = [my_list[i] for i in range(len(my_list)) if my_list[i] not in new_list]
 print(new_list)
+
