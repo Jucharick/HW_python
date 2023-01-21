5110167040586b3280027662554161aaa74f7ef5
Julia Ch <juliecha@yandex.ru>
2023-01-21 20:36:46 +0300

    UPD Task_020 - add list comprehension

--- Task_020.py
+++ Task_020.py
@@ -6,11 +6,17 @@
 import math
 from random import randint
 
+# number = int(input('Введите число элементов в списке: '))
+# my_list = []
+# for i in range(number):
+#     my_list.append(randint(0, 7))
+# res = []
+# for i in range(math.ceil((number/2))): # math.ceil() округление в большую сторону
+#     res.append(my_list[i]*my_list[number - 1 - i])
+# print(f'{my_list}  =>  {res}')
+
 number = int(input('Введите число элементов в списке: '))
-my_list = []
-for i in range(number):
-    my_list.append(randint(0, 7))
-res = []
-for i in range(math.ceil((number/2))): # math.ceil() округление в большую сторону
-    res.append(my_list[i]*my_list[number - 1 - i])
+my_list = [randint(0, 7) for _ in range(number)]
+res = [my_list[i]*my_list[number - 1 - i] for i in range(math.ceil((number/2)))]
+
 print(f'{my_list}  =>  {res}')
