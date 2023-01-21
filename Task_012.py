2561fa88927234e9f49c946d2877e6ae63f5fc2f
Julia Ch <juliecha@yandex.ru>
2023-01-21 19:48:52 +0300

    UPD Task_012 - add list comprehension

--- Task_012.py
+++ Task_012.py
@@ -3,12 +3,20 @@
 # Для n=4 -> [2, 2.25, 2.37, 2.44]
 # Сумма 9.06
 
-num = int(input('Введите число элементов: '))
-my_list = []
-sum = 0
-for i in range(1, num+1): # на 0 делить нельзя
-    my_list.append(round((1 + 1/i)**i, 2))
-    sum = sum + (1 + 1/i)**i
+# num = int(input('Введите число элементов: '))
+# my_list = []
+# sum = 0
+# for i in range(1, num+1): # на 0 делить нельзя
+#     my_list.append(round((1 + 1/i)**i, 2))
+#     sum = sum + (1 + 1/i)**i
 
+# print(f'Для n = {num} => {my_list}')
+# print(f'Сумма элементов => {round(sum, 2)}')
+
+# Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension.
+
+num = int(input('Введите число элементов: '))
+my_list = [round((1 + 1/i)**i, 2) for i in range(1, num+1)]
+sum_elem = sum(my_list )
 print(f'Для n = {num} => {my_list}')
-print(f'Сумма элементов => {round(sum, 2)}')
+print(f'Сумма элементов => {round(sum_elem, 2)}')
