2e6731a5c33e7aa3b714dbb243067cde744554ef
Julia Ch <juliecha@yandex.ru>
2023-01-31 21:54:37 +0300

    UPD telebot

--- telebot/handlers.py
+++ telebot/handlers.py
@@ -91,7 +91,7 @@ def bot_turn(message):
                                           f'На столе осталось {total} конфет и Умный бот победил')
         new_game = False
     elif total%(28+1) == 0:
-        bot_take = 28-1
+        bot_take = 28
         total -= bot_take
         bot.send_message(message.chat.id, f'Умный бот взял {bot_take} конфет. '
                                           f'На столе осталось {total} конфет. ')
@@ -103,3 +103,53 @@ def bot_turn(message):
 
 
 bot.infinity_polling() # бот проверяет не пришли ли какие-то сообщения    
+
+
+
+# #на столе лежит...
+# import telebot
+# import random
+
+# bot = telebot.TeleBot("6185618554:AAGS8T5BvicTGRTkGKp3HGAq0HPYLnln8UM")
+
+# flag = None
+# sweets = 60
+# max_sweet = 28
+
+# @bot.message_handler(commands=["start"])
+# def start(message):
+#     global flag
+#     bot.send_message(message.chat.id, f"Приветствую вас в игре!")
+#     flag = random.choice(["user", "bot"])
+#     bot.send_message(message.chat.id, f"Всего в игре {sweets} конфет")
+#     if flag == "user":
+#         bot.send_message(message.chat.id,f"Первым ходите вы")  # отправка сообщения (кому отправляем, что отправляем(str))
+#     else:
+#         bot.send_message(message.chat.id,f"Первым ходит бот")# отправка сообщения (кому отправляем, что отправляем(str))
+#     controller(message)
+
+# def controller(message):
+#     global flag
+#     if sweets > 0 :
+#         if flag == "user":
+#             bot.send_message(message.chat.id, f"Ваш ход введите кол-во конфет от 0 до {max_sweet}")
+#             bot.register_next_step_handler(message,user_input)
+#         else:
+#             bot_input(message)
+#     else:
+#         flag = "user" if flag == "bot" else "bot"
+#         bot.send_message(message.chat.id, f"Победил {flag}!")
+
+# def bot_input(message):
+#     global sweets, flag
+#     if sweets <= max_sweet:
+#         bot_turn = sweets
+#     elif sweets % max_sweet == 0:
+#         bot_turn = max_sweet - 1
+#     else:
+#         bot_turn = sweets % max_sweet - 1
+#     sweets -= bot_turn
+#     bot.send_message(message.chat.id, f"бот взял {bot_turn} конфет")
+#     bot.send_message(message.chat.id, f"осталось {sweets}")
+#     flag = "user" if flag == "bot" else "bot"
+#     controller(message)
